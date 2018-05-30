0af4f5d0ce6ca1203f790539c4da9460870858cf
tashiwangmo <tsultrimlhewa@tsultrims-air.ad.ascendlearning.com>
2018-05-30 17:52:34 -0500

    a

--- Psychic-Game/assets/javascript/game.js
+++ Psychic-Game/assets/javascript/game.js
@@ -1,8 +1,8 @@
 
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var wins = 0;
-var losses = 0;
+var wins = 0 ;
+var losses = 0 ;
 var guesses = 9;
 var guessesLeft = 9;
 var guessedLetters = [];
